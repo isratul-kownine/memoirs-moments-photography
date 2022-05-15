513c043c5334d12d8981e94ba8e8ecd3a6cf8f05
isratul-kownine <isratulkownine222@gmail.com>
2022-05-15 11:26:44 +0600

    services page got ready

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './Service.css';
 
 const Service = ({service}) => {
-    const {name , price} = service;
+    const {name , price, picture,about} = service;
     return (
-        <div>
-             <h2>price : {price}</h2>
+        <div className='service'>
+            
+             <img src={picture} alt="" />
+             <h3>{name}</h3>
+             <p>Price : $ <small>{price}</small></p>
+             <p>{about}</p>
+             <button> Book : {name}</button>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
 
@@ -14,14 +15,16 @@ const Services = () => {
 
     return (
         <div>
-            <h2>Services :{services.length}</h2>
+            <h1>Our Services</h1>
 
+            <div className='services-container'>
             {
                 services.map(service => <Service
                          key={service._id}
                          service ={service}
                     ></Service>)
             }
+            </div>
         </div>
     );
 };
